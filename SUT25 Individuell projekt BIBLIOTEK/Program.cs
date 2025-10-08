907a37b117c021d1bf240f102a59b5a87e927e62
Bellanthie <bellanthie@gmail.com>
2025-10-08 15:15:22 +0200

    Add project files.

---
+++ SUT25 Individuell projekt BIBLIOTEK/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUT25_Individuell_projekt_BIBLIOTEK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till detta lånesystem!");
+            Console.WriteLine("Vänligen skriv in ditt användarnamn och PIN kod:");
+            Console.ReadLine();
+
+
+
+
+        }
+
+        // Creating a method to welcome user to the Library system.
+        // Incorporating that user has max 3 attempts to log in.
+        static int userLogIn()
+        {
+            Console.Clear(); // gives the user a fresh start if they've written the wrong PIN. When they restart the login screen, we want a new clean message.
+            Console.WriteLine("Välkommen till bibliotekets lånesystem!\n"); // by adding \n, we create another space EVEN after CW giving a cleaner look.
+
+
+        }
+    }
+}
+
