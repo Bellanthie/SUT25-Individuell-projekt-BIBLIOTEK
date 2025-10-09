188289bb3217dc8ae65e059fdb1376d7a38543f0
Bellanthie <bellanthie@gmail.com>
2025-10-09 10:51:57 +0200

    tyring to figure out which static 'mode' to use for my user login and pincod 251009

--- SUT25 Individuell projekt BIBLIOTEK/Program.cs
+++ SUT25 Individuell projekt BIBLIOTEK/Program.cs
@@ -8,12 +8,19 @@ namespace SUT25_Individuell_projekt_BIBLIOTEK
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            /* static void SayHello()
+             * {
+             *      Console.WriteLine("Hej där!");
+             *      *** EVERYTHING STARTS HERE. THE PROGRAM STARTS HERE. ALWAYS
+             * */
+
             Console.WriteLine("Välkommen till detta lånesystem!");
             Console.WriteLine("Vänligen skriv in ditt användarnamn och PIN kod:");
             Console.ReadLine();
-
+            userLogIn();
 
 
 
@@ -21,7 +28,7 @@ namespace SUT25_Individuell_projekt_BIBLIOTEK
 
         // Creating a method to welcome user to the Library system.
         // Incorporating that user has max 3 attempts to log in.
-        static int userLogIn()
+        static void userLogIn()
         {
             Console.Clear(); // gives the user a fresh start if they've written the wrong PIN. When they restart the login screen, we want a new clean message.
             Console.WriteLine("Välkommen till bibliotekets lånesystem!\n"); // by adding \n, we create another space EVEN after CW giving a cleaner look. En visuell andnings paus för användaren.
